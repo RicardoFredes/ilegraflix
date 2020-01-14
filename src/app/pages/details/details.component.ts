926e8b97f1464a815cef84e249377f1bd83a7997
Ricardo Fredes <ricardofredes@gmail.com>
2020-01-13 23:15:02 -0200

    instance movie details component

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../../services/movies.service'
+import { Movie } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  movie: Movie;
 
-  constructor() { }
+  constructor(
+    private moviesService: MoviesService,
+    private route: ActivatedRoute,
+  ) {
+    const movieId = this.route.snapshot.paramMap.get('movie')
+    const id = Number(movieId)
+    this.movie = this.moviesService.getMovie(id)
+   }
 
   ngOnInit() {
+    console.log(this.movie)
   }
 
 }
